dd92abd69361764012fab0bcebc7848c95b2689f
Luis David <luisdaviduacm@gmail.com>
2018-12-21 10:46:25 -0600

    Primer avance

---
+++ src/main/java/mx/com/xhani/modelo/Autor.java
@@ -0,0 +1,71 @@
+package mx.com.xhani.modelo;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+@Entity
+public class Autor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nombre;
+	private String apellidos;
+	@ManyToMany(mappedBy = "autores")
+	private Set<Libro> libros;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public Set<Libro> getLibros() {
+		return libros;
+	}
+	public void setLibros(Set<Libro> libros) {
+		this.libros = libros;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Autor [id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", libros=" + libros + "]";
+	}
+	
+}
---
+++ src/main/java/mx/com/xhani/modelo/Libro.java
@@ -0,0 +1,70 @@
+package mx.com.xhani.modelo;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Libro {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nombre;
+	@ManyToMany
+	@JoinTable(name="autor_libro",joinColumns = @JoinColumn(name= "libro_id"),
+	inverseJoinColumns = @JoinColumn(name = "autor_id"))
+	private Set<Autor> autores;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Set<Autor> getAutores() {
+		return autores;
+	}
+	public void setAutores(Set<Autor> autores) {
+		this.autores = autores;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Libro other = (Libro) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Libro [id=" + id + ", nombre=" + nombre + ", autores=" + autores + "]";
+	}
+	
+}
